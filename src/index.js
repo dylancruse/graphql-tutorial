efafe1df11c207a2f5e900ea40f6fae0b2eb072e
Dylan Cruse <dylancruse@live.com>
2019-10-17 19:14:27 -0500

    Create Apollo Server, express middleware, and basic gql schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-require('dotenv').config({ path: '.env' });
+const cors = require('cors');
 
-console.log(process.env.TEST);
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const express = require('express');
+
+const app = express();
+
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => ({
+      username: 'Dylan Cruse',
+    }),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server is running on http://localhost:8000/graphql');
+});
