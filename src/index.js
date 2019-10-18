f4abf6151ace6170df27f7e9a6ec27ebfd3fd61f
Dylan Cruse <dylancruse@live.com>
2019-10-17 20:48:46 -0500

    Add Message type to schema, add resolvers, add sample messages

--- src/index.js
+++ src/index.js
@@ -11,22 +11,47 @@ const schema = gql`
     me: User
     user(id: ID!): User
     users: [User!]
+
+    message(id: ID!): Message
+    messages: [Message!]
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `;
 
 const users = {
   1: {
-    id: '100',
+    id: '1',
     username: 'Dylan Cruse',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Kate Larberg',
+    messageIds: [2],
+  },
+};
+
+const messages = {
+  1: {
+    id: '1',
+    text: 'Hello World :)',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Goodbye World :(',
+    userId: '2',
   },
 };
 
@@ -35,9 +60,16 @@ const resolvers = {
     me: (parent, args, { me }) => me,
     user: (parent, { id }) => users[id],
     users: () => Object.values(users),
+    message: (parent, { id }) => messages[id],
+    messages: () => Object.values(messages),
   },
   User: {
     username: user => user.username.slice(0, 5),
+    messages: user =>
+      Object.values(messages).filter(message => user.id === message.userId),
+  },
+  Message: {
+    user: message => users[message.userId],
   },
 };
 
