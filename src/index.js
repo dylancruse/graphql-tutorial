1dce13ec2cb0114844f8d6cbfed44966efc28781
Dylan Cruse <dylancruse@live.com>
2019-10-17 21:01:11 -0500

    Split sample data to separate file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
 const cors = require('cors');
+const { users, messages } = require('../sampleData');
 
 const app = express();
 
@@ -28,32 +29,6 @@ const schema = gql`
     user: User!
   }
 `;
-
-const users = {
-  1: {
-    id: '1',
-    username: 'Dylan Cruse',
-    messageIds: [1],
-  },
-  2: {
-    id: '2',
-    username: 'Kate Larberg',
-    messageIds: [2],
-  },
-};
-
-const messages = {
-  1: {
-    id: '1',
-    text: 'Hello World :)',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'Goodbye World :(',
-    userId: '2',
-  },
-};
 
 const resolvers = {
   Query: {
