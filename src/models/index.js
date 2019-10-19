52aa05f775964e9e1be8afa116b06226c964ad62
Dylan Cruse <dylancruse@live.com>
2019-10-19 17:00:08 -0500

    Code cleanup

--- src/models/index.js
+++ src/models/index.js
@@ -1,31 +1,3 @@
-// const users = {
-//   1: {
-//     id: '1',
-//     username: 'Dylan Cruse',
-//     messageIds: [1],
-//   },
-//   2: {
-//     id: '2',
-//     username: 'Kate Larberg',
-//     messageIds: [2],
-//   },
-// };
-
-// const messages = {
-//   1: {
-//     id: '1',
-//     text: 'Hello World :)',
-//     userId: '1',
-//   },
-//   2: {
-//     id: '2',
-//     text: 'Goodbye World :(',
-//     userId: '2',
-//   },
-// };
-
-// export default { users, messages };
-
 import Sequelize from 'sequelize';
 import 'dotenv/config';
 
