e35c79a0441538f85fc3ca341135a09ce1ebfa2e
Dylan Cruse <dylancruse@live.com>
2019-10-19 17:51:01 -0500

    Create user signIn function

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-return-await */
 import jwt from 'jsonwebtoken';
+import { AuthenticationError, UserInputError } from 'apollo-server';
 
 const createToken = async (user, secret, expiresIn) => {
   const { id, email, username } = user;
@@ -28,6 +29,21 @@ export default {
         email,
         password,
       });
+      return { token: createToken(user, secret, '30m') };
+    },
+    signIn: async (parent, { login, password }, { models, secret }) => {
+      const user = await models.User.findByLogin(login);
+
+      if (!user) {
+        throw new UserInputError('No user found with these login credentials.');
+      }
+
+      const isValid = await user.validatePassword(password);
+
+      if (!isValid) {
+        throw new AuthenticationError('Invalid password.');
+      }
+
       return { token: createToken(user, secret, '30m') };
     },
   },
