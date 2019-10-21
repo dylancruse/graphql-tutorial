63470b9cb41ddebf1c2f6ca3d93be4952ac0e807
Dylan Cruse <dylancruse@live.com>
2019-10-20 20:59:09 -0500

    Implement pagination in messages query using hashing

--- src/schema/message.js
+++ src/schema/message.js
@@ -2,14 +2,24 @@ import { gql } from 'apollo-server-express';
 
 export default gql`
   extend type Query {
+    messages(cursor: String, limit: Int): MessageConnection!
     message(id: ID!): Message
-    messages: [Message!]
   }
 
   extend type Mutation {
     createMessage(text: String!): Message!
     deleteMessage(id: ID!): Boolean!
     updateMessage(id: ID!, text: String!): Boolean!
+  }
+
+  type MessageConnection {
+    edges: [Message!]!
+    pageInfo: PageInfo!
+  }
+
+  type PageInfo {
+    hasNextPage: Boolean!
+    endCursor: String!
   }
 
   type Message {
